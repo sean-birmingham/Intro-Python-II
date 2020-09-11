9ec7f620421f49ff7822620c04f970d65e151d18
Sean Birmingham <seannbirmingham98@gmail.com>
2020-09-11 18:36:25 -0400

    need to fix some things, item pickup and drop working

--- src/player.py
+++ src/player.py
@@ -4,3 +4,22 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
+
+    def take_item(self, item):
+        self.inventory.append(item)
+        item.on_take()
+        self.current_room.items.remove(item)
+
+    def drop_item(self, item):
+        self.inventory.remove(item)
+        item.on_drop()
+        self.current_room.items.append(item)
+
+    def show_inventory(self):
+        if len(self.inventory) < 1:
+            print('There are no items in your inventory')
+        else:
+            print('You are carrying:')
+            for item in self.inventory:
+                print(item.name)
